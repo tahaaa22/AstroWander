3e90a32958aabef9d97d6dd23a0697efea66b164
Amira El-Garf <elgarfamira4@gmail.com>
2023-10-28 13:46:22 +0200

    patch7 120

---
+++ AstroWander/Assets/Mars.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
+
+public class Mars : MonoBehaviour
+{
+    private XRGrabInteractable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+        interactable.onSelectEntered.AddListener(OnClick);
+
+    }
+    private void OnClick(XRBaseInteractor interactor)
+    {
+        // This function will be called when the object is clicked
+        // Add your desired behavior here
+        //Debug.Log("Object clicked!");
+
+        SceneManager.LoadScene(1);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
